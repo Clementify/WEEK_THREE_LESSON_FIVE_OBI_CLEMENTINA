f3d9c389a87d2c5c60178a7768b6d8daf6b9fa28
clementify <obiclementina92@gmail.com>
2023-07-13 15:43:59 +0100

    task

--- tasks/task_five.js
+++ tasks/task_five.js
@@ -4,3 +4,11 @@
 // Use the filter() method to create a new array containing only specific elements based on a condition.
 // Log the filtered array to the console.
 
+const days= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const filteredDays = days.filter( num =>{
+    if (num ===3 || num ===6) {
+        return num
+    }
+}) 
+console.log(filteredDays)
+// output [3, 6]
