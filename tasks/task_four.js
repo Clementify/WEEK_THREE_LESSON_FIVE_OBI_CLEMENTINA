81251167d4d32c6ba59655add0962e4a09e02ffa
clementify <obiclementina92@gmail.com>
2023-07-13 15:22:58 +0100

    task

--- tasks/task_four.js
+++ tasks/task_four.js
@@ -3,3 +3,20 @@
 // Create an array with multiple elements.
 // Use a loop (such as for or forEach()) to iterate over the array.
 // Perform an action on each element, such as logging it to the console.
+
+const numbers = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+numbers.forEach((num) => {
+  console.log(num)
+})
+
+// logs => 21
+//         22
+//         23
+//         24
+//         25
+//         26
+//         27
+//         28
+//         29
+//         30
