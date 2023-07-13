fb526a953ccba2cdab1e475abf2b80ef12304585
clementify <obiclementina92@gmail.com>
2023-07-13 15:11:45 +0100

    task

--- tasks/task_three.js
+++ tasks/task_three.js
@@ -5,3 +5,9 @@
 // Modify the element value using assignment.
 // Log the array to the console to see the updated value.
 
+const days= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+days[10] = '11'
+console.log(days)
+
+// logs => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
