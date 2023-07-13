c10eb92fdbba19734f8ccbe32f23a76c3db314a0
clementify <obiclementina92@gmail.com>
2023-07-13 15:05:36 +0100

    task

--- tasks/task_two.js
+++ tasks/task_two.js
@@ -4,3 +4,12 @@
 // Use the pop() method to remove the last element from the array.
 // Use the shift() method to remove the first element from the array.
 // Log the array to the console to see the updated elements.
+
+const days= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const pop = days.pop()
+const shift = days.shift()
+
+console.log(days)
+
+// output logs => [2, 3, 4, 5, 6, 7, 8, 9]
